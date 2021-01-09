6150da69316a4229eae6c112ea1cbc3f592b275b
MekanYaldyrov <Mekan.yaldyrov@phystech.edu>
2021-01-09 15:32:09 +0300

    readding fixed prime.py

--- exam/prime.py
+++ exam/prime.py
@@ -1,13 +1,18 @@
-def prime_gen(limit):
-    for i in range(2, limit):
-        j = 2
-        while j < i:
-            if i % j == 0:
-                break
-            j += 1
-        else:
-            yield i
+def IsPrime(n):
+    d = 2
+    while d * d <= n and n % d != 0:
+        d += 1
+    return d * d > n
 
 
-a = int(input('Введите число, до которго будет проводиться поиск\n'))
-print(list(prime_gen(a)))
+def prime_gen():
+    num = 2
+    while True:
+        if IsPrime(num):
+            yield num
+        num += 1
+
+
+k = prime_gen()
+while True:
+    print(next(k))
